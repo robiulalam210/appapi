0f1976cf91849e20d5ced31fb97158cf7350e0d9
robiul210 <robiulstudies@gmail.com>
2024-01-11 21:38:19 +0600

    code change

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->string('name');
+            $table->date('dob')->nullable();
+            $table->string('blood_group');
+            $table->string('gender');
+            $table->string('religion');
+            $table->string('nid_smart');
+            $table->string('images')->nullable();
             $table->string('mobile');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
